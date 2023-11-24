5c81b19c8f1df112eeac14a696900f630cb36fb8
JhonnyVelascoM <jvelascom5@est.ups.edu.ec>
2023-11-24 11:41:09 -0500

    primer commit
    Creación de un microservicio

---
+++ src/test/java/com/siatigroup/gop/empleados/prueba_microservicio/PruebaMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siatigroup.gop.empleados.prueba_microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
